a551bc496a86f8aa7b5ec0a540a556c1f1635340
VISTALL <vistall@i.ua>
2012-08-18 22:06:42 +0300

    kotlin -> napile

--- idea/src/org/jetbrains/jet/plugin/actions/NewKotlinFileAction.java
+++ idea/src/org/jetbrains/jet/plugin/actions/NewKotlinFileAction.java
@@ -42,7 +42,7 @@ public class NewKotlinFileAction extends CreateFileFromTemplateAction
 	@Override
 	protected void buildDialog(Project project, PsiDirectory directory, CreateFileFromTemplateDialog.Builder builder)
 	{
-		builder.setTitle("Create New Class").addKind("Class", JetIcons.CLASS, "Kotlin Class").addKind("Enum", JetIcons.ENUM, "Kotlin Enum");
+		builder.setTitle("Create New Class").addKind("Class", JetIcons.CLASS, "Napile Class").addKind("Enum", JetIcons.ENUM, "Napile Enum");
 	}
 
 	@Override
