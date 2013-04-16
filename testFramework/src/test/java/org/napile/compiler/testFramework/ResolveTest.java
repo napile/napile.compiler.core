40f0d5113aabdf649da41c1ad1ad10b52b25475e
VISTALL <vistall.valeriy@gmail.com>
2013-04-16 16:39:51 +0300

    resolve tests

---
+++ testFramework/src/test/java/org/napile/compiler/testFramework/ResolveTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2010-2013 napile.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.napile.compiler.testFramework;
+
+import org.napile.compiler.lang.descriptors.DeclarationDescriptor;
+import org.napile.compiler.lang.psi.NapileFile;
+import org.napile.compiler.lang.psi.NapileReferenceExpression;
+import org.napile.compiler.lang.resolve.BindingTraceKeys;
+import org.napile.compiler.lang.resolve.DescriptorUtils;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+
+/**
+ * @author VISTALL
+ * @since 16:22/16.04.13
+ */
+public class ResolveTest extends NapileMassTestCase
+{
+	@Override
+	public String getExpectedText(NapileFile file) throws Exception
+	{
+		StringBuilder builder = new StringBuilder();
+
+		walk(file.getNode(), builder);
+
+		return builder.toString();
+	}
+
+	private void walk(ASTNode node, StringBuilder builder)
+	{
+		ASTNode temp = node.getFirstChildNode();
+		while(temp != null)
+		{
+			appendText(builder, temp);
+
+			temp = temp.getTreeNext();
+		}
+	}
+
+	private void appendText(StringBuilder builder, ASTNode temp)
+	{
+		PsiElement element = temp.getPsi();
+		if(element instanceof NapileReferenceExpression)
+		{
+			final DeclarationDescriptor declarationDescriptor = analyzeExhaust.getBindingTrace().get(BindingTraceKeys.REFERENCE_TARGET, (NapileReferenceExpression) element);
+
+			String ref = null;
+			if(declarationDescriptor == null)
+			{
+				ref = "null";
+			}
+			else
+			{
+				ref = declarationDescriptor.getClass().getSimpleName() + "[" + DescriptorUtils.getFQName(declarationDescriptor).getFqName() + "]";
+			}
+
+			builder.append("<").append(ref).append(">");
+
+			walk(temp, builder);
+
+			builder.append("</").append(ref).append(">");
+		}
+		else
+		{
+			if(temp.getFirstChildNode() == null)
+			{
+				builder.append(temp.getText());
+			}
+			else
+			{
+				walk(temp, builder);
+			}
+		}
+	}
+
+
+	@Override
+	public String getResultExt()
+	{
+		return "resolve";
+	}
+}
